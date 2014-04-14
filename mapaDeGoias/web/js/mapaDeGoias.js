307324c2ceea806ec914d8ff4e5934267611f36f
brunorfranco <brunozo_rf@hotmail.com>
2014-04-14 20:57:14 -0300

    Adicionando plugin maphilight
    Adicionando plugin maphilight

--- mapaDeGoias/web/js/mapaDeGoias.js
+++ mapaDeGoias/web/js/mapaDeGoias.js
@@ -2,9 +2,19 @@ function setOnMouseHoverCidades() {
 	 var inputs = getInputElements('idsCidades');
 	 
 	 for(var i=0; i<inputs.length; i++){
+//		 var id = inputs[i].value;
 		 var idCidade = inputs[i].value.split("|")[0];
 		 var nomeCidade = inputs[i].value.split("|")[1];
 		 var populacaoCidade = inputs[i].value.split("|")[2];
+		 var resposta = 'cidade'+idCidade;
+		 $(idCidade).maphilight();
+//		 $(id).maphilight();
+		 
+		 $(idCidade).mouseover(function(e) {
+			    $(idCidade).mouseover();
+			}).mouseout(function(e) {
+			    $(idCidade).mouseout();
+			}).click(function(e) { e.preventDefault(); });
 	 }
 }
 
